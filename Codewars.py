9cb85a573a51910d2ff4426beeaad2faad394a61
Botos Bence <botosbence01@gmail.com>
2023-09-24 22:38:23 +0200

    Save

--- Codewars.py
+++ Codewars.py
@@ -34,3 +34,10 @@
 #     return " + ".join(b)
 #
 # print(expanded_form(70304))
+
+
+# def find_needle(haystack):
+#     return f"found the needle at position {haystack.index('needle')}"
+#
+#
+# print(find_needle(['3', '123124234', None, 'needle', 'world', 'hay', 2, '3', True, False]))
